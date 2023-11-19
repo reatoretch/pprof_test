6d51b9cdfc09a098a0dd50a3b7e0c5c26d3e319f
EC2 Default User <ec2-user@ip-172-31-16-38.ap-northeast-1.compute.internal>
2023-11-19 08:29:10 +0000

    test pprof

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    _ "net/http/pprof"
+)
+
+func fibo(a int) int {
+    if a<=1 {
+        return 1
+    } else {
+        return fibo(a-1) + fibo(a-2)
+    }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    // w.Write([]byte("Hello, World"))
+    w.Write([]byte(string(fibo(30))))
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Println(http.ListenAndServe("localhost:6060", nil))
+}
